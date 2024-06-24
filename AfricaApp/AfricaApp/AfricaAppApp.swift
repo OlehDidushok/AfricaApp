703f3cd814a7fdd67ae2166590491d20b60dd409
Oleh Didushok <o.didushok@lampalampa.net>
2024-06-24 21:25:44 +0300

    Initial commit

---
+++ AfricaApp/AfricaApp/AfricaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaAppApp.swift
+//  AfricaApp
+//
+//  Created by Oleh on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct AfricaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
