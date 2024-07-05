7ae135aada18342969b2e603239f23f9e9a3ecd7
Oleh Didushok <o.didushok@lampalampa.net>
2024-07-05 15:40:32 +0300

    Initial configuration (added images, colors, created main Views)

---
+++ AfricaApp/AfricaApp/App/GalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryView.swift
+//  AfricaApp
+//
+//  Created by Oleh on 05.07.2024.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text("Gallery")
+    }
+}
+
+#Preview {
+    GalleryView()
+}
---
+++ AfricaApp/AfricaApp/App/VideoListView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoListView.swift
+//  AfricaApp
+//
+//  Created by Oleh on 05.07.2024.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+#Preview {
+    VideoListView()
+}
